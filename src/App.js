d8e42ba4e453d6016ff6e8b1e74208e2f8bd337c
Cameron Alexis <camer@MacBook-Air-5.local>
2021-02-03 20:00:31 -0800

    homework turnin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {useState} from 'react';
+import TextInput from './TextInput';
+import Message from './Message';
+
 function App() {
+  const [messages, setMessages] = useState([])
+ 
   return <div className="App">
+
     <header className="header">
       <div className="logo" />
       CHATTER
     </header>
+
+    <main className="messages">
+      {messages.map((m,i)=> {
+        return <Message key={i}{...m} />
+      })}
+    </main>
+
+    <TextInput
+    send={t=> console.log(t)}
+    />
+    <TextInput  
+      send={(t)=> setMessages( [{text:t}, ...mesages] )}
+    />
+
   </div>
 }
 export default App;
